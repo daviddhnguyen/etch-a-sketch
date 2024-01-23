2ed1ef366cd6802eedad3db58eb50592cb033dac
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-22 21:12:14 -0800

    Update cell naming and add grid border

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const grid = document.querySelector('.grid');
+//prompt user for grid dimensions and clears board
+const btnResize = document.querySelector('.btnResize');
 
 let gridItemsPerColumn = 16;
+
+function userSquareInput() {
+  gridItemsPerColumn = prompt('Enter # of squares per side', 16);
+  return gridItemsPerColumn
+}
+
+//create square grid with boxes defined by size and items
+
+const grid = document.querySelector('.grid');
+
 let totalBoxes = gridItemsPerColumn**2;
 
-let gridWidth = 960;
+let gridWidth = 600;
 let gridHeight = gridWidth;
 
 grid.style.width = `${gridWidth}px`;
 grid.style.height = `${gridHeight}px`;
+grid.style.flexBasis = `${gridHeight}px`;
+grid.style.border = 'solid 5px blue';
 
-let boxSize = gridWidth/gridItemsPerColumn;
+let cellSize = gridWidth/gridItemsPerColumn;
 
 console.log(totalBoxes);
 
@@ -27,16 +40,29 @@ for (let i = 1; i <= totalBoxes; i++) {
   //console.log(grid);
 }
 
+
+//standardize box sizes
 const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
-  box.style.flex = `1 1 ${boxSize}px`;
-  box.style.width = `${boxSize}px`;
-  box.style.height = `${boxSize}px`;
+boxes.forEach((cell) => {
+  cell.style.flex = `1 1 ${cellSize}px`;
+  cell.style.width = `${cellSize}px`;
+  cell.style.height = `${cellSize}px`;
 })
+
+//create colored boxes when mouse hovers
 
 boxes.forEach((item) => {
   item.addEventListener('mouseover', () => {
     item.className += ' coloredBox';
+  });
+});
+
+//create button to clear cells
+const btnClear = document.querySelector('.btnClear');
+
+btnClear.addEventListener('click', () => {
+  boxes.forEach((item) => {
+    item.className -= 'coloredBox';
   });
 });
