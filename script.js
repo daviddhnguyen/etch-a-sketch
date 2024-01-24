aef5ac8874ec846fa4cf9951f6f1ec862413efd6
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-23 21:51:33 -0800

    Refactor and remove old createGrid

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let currentColor = DEFAULT_COLOR;
 
 // event listeners
 
-// create grid with total number of cells
+// create grid defined by # of cells per row and default width
 function createGrid(cellCount) {
   const grid = document.querySelector('.grid');
   let totalCells = cellCount**2;
@@ -56,8 +56,6 @@ function createGrid(cellCount) {
 
 /*
 
-
-
 function userSquareInput() {
   let whileTrue = true;
 
@@ -80,51 +78,6 @@ const btnResize = document.querySelector('.btnResize');
 btnResize.addEventListener('click', () => {
   createGrid(userSquareInput());
 });
-
-/*
-//create square grid with boxes defined by size and items
-function createGrid(gridItemsPerColumn) {
-  const grid = document.querySelector('.grid');
-
-  let totalBoxes = gridItemsPerColumn**2;
-
-  let gridWidth = 600;
-  let gridHeight = gridWidth;
-
-  grid.style.width = `${gridWidth}px`;
-  grid.style.height = `${gridHeight}px`;
-  grid.style.flexBasis = `${gridHeight}px`;
-  grid.style.border = 'solid 5px blue';
-
-  let cellSize = gridWidth/gridItemsPerColumn;
-
-  console.log(totalBoxes);
-
-  for (let i = 1; i <= totalBoxes; i++) {
-    const box = document.createElement('div');
-    
-    box.setAttribute('class',"box");
-    
-    //const node = document.createTextNode(i);
-    //box.appendChild(node);
-    
-    box.style.border = 'solid 1px blue';
-    
-    grid.appendChild(box);
-    //console.log(grid);
-  }
-  
-  const boxes = document.querySelectorAll('.box');
-
-  boxes.forEach((cell) => {
-    cell.style.flex = `1 1 ${cellSize}px`;
-    cell.style.width = `${cellSize}px`;
-    cell.style.height = `${cellSize}px`;
-  })
-}
-
-//standardize box sizes
-
 
 //create colored boxes when mouse hovers
 const boxes = document.querySelectorAll('.box');
