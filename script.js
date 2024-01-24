94d7969dad8a72b52923e301cd73f4eefe55f50a
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-23 21:37:32 -0800

    Adds function to createGrid

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
-const DEFAULT_SIZE = 16
-const DEFAULT_COLOR = 'blue';
+/* create grid using flex box
+- define constants: size of grid,
+- define defaults: # of cells per row, color of cell
+- use JS to create total # of divs.
+  - calculate total number of divs by inputing # of cells per row and square the number
+  - establish # of cells per row by dividing width of grid by # of cells per row. 
+- create function to recolor a cell over mouse over by assigning additional class to div
+- inputing # to change # of cells per row
+  - remove all previous divs
+  - create total # of divs and calculate cells
+*/
 
-let currentSize = DEFAULT_SIZE;
+const DEFAULT_CELL_COUNT = 16
+const DEFAULT_COLOR = 'blue';
+const DEFAULT_WIDTH = 960;
+
+let currentCellCount = DEFAULT_CELL_COUNT;
+let currentColor = DEFAULT_COLOR;
+
+// event listeners
+
+// create grid with total number of cells
+function createGrid(cellCount) {
+  const grid = document.querySelector('.grid');
+  let totalCells = cellCount**2;
+  let cellSize = DEFAULT_WIDTH/cellCount;
+
+  for (let i = 1; i <= totalCells; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('class','cell')
+    cell.style.border = 'solid blue 1px';
+    grid.appendChild(cell);
+  }
+
+  grid.style.width = `${DEFAULT_WIDTH}px`;
+  grid.style.height = `${DEFAULT_WIDTH}px`;
+  grid.style.flexBasis = `${DEFAULT_WIDTH}px`;
+
+  const cells = document.querySelectorAll('.cell');
+
+  cells.forEach((cell) => {
+    cell.style.flex = `1 1 ${cellSize}px`;
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
+  });
+};
+
+// delete current grid
+
+
+//add new style class to cell
+
+
+
+//clear new style class from cell
+
+/*
+
+
 
 function userSquareInput() {
   let whileTrue = true;
@@ -26,6 +81,7 @@ btnResize.addEventListener('click', () => {
   createGrid(userSquareInput());
 });
 
+/*
 //create square grid with boxes defined by size and items
 function createGrid(gridItemsPerColumn) {
   const grid = document.querySelector('.grid');
@@ -87,7 +143,8 @@ btnClear.addEventListener('click', () => {
     item.className -= 'coloredBox';
   });
 });
+*/
 
 window.onload = () => {
-  createGrid(DEFAULT_SIZE)
-}
+  createGrid(DEFAULT_CELL_COUNT)
+};
